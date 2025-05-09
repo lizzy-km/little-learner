007a9a542dfbd98b92fd68294dbf5b07696d6e65
lizzy-km <oxygenisme.02@gmail.com>
2025-05-09 23:53:15 +0630

    refactor

---
+++ src/dummy.jsx
@@ -0,0 +1,171 @@
+ 
+import IconOne from "../src/assets/Icon_01.svg?react";
+import IconTwo from "../src/assets/Icon_02.svg?react";
+import IconThree from "../src/assets/Icon_03.svg?react";
+import IconFour from "../src/assets/Icon_04.svg?react";
+import IconFive from "../src/assets/Icon_05.svg?react";
+import IconSix from "../src/assets/Icon_06.svg?react";
+import IconPfOne from "../src/assets/Icon_pf_01.svg?react";
+import IconPfTwo from "../src/assets/Icon_pf_02.svg?react";
+import IconPfThree from "../src/assets/Icon_pf_03.svg?react";
+
+ export const cardsData1 = [
+    {
+      icon: <IconOne />,
+      title: "Holistic Learning Approach",
+      description:
+        "Our curriculum focuses on nurturing cognitive, social, emotional, and physical development, ensuring a well-rounded education.",
+      id: 1,
+    },
+    {
+      icon: <IconTwo />,
+      title: "Experienced Educators",
+      description:
+        "Our passionate and qualified teachers create a supportive and stimulating learning environment.",
+      id: 2,
+    },
+    {
+      icon: <IconThree />,
+      title: "Nurturing Environment",
+      description:
+        "We prioritize safety and provide a warm and caring atmosphere for every child.",
+      id: 3,
+    },
+  ];
+
+ export const cardsData2 = [
+    {
+      icon: <IconFour />,
+      title: "Play-Based Learning",
+      description:
+        "We believe in the power of play to foster creativity, problem-solving skills, and imagination.",
+      id: 1,
+    },
+    {
+      icon: <IconFive />,
+      title: "Individualized Attention",
+      description:
+        "Our small class sizes enable personalized attention, catering to each child's unique needs.",
+      id: 2,
+    },
+    {
+      icon: <IconSix />,
+      title: "Parent Involvement",
+      description:
+        "We foster a strong parent-school partnership to ensure seamless communication and collaboration.",
+      id: 3,
+    },
+  ];
+
+  export const avatarData1 = [
+    {
+      icon: <IconPfOne />,
+      title: "Jennifer B",
+      description:
+        "Little Learners Academy has been a second home for my child. The caring staff and engaging programs have made her excited to go to school every day!",
+      id: 1,
+    },
+    {
+      icon: <IconPfTwo />,
+      title: "Experienced Educators",
+      description:
+        "Our passionate and qualified teachers create a supportive and stimulating learning environment.",
+      id: 2,
+    },
+    {
+      icon: <IconPfThree />,
+      title: "Nurturing Environment",
+      description:
+        "We prioritize safety and provide a warm and caring atmosphere for every child.",
+      id: 3,
+    },
+  ];
+
+ export const faqData1 = [
+    {
+      title: "What are the school hours at Little Learners Academy?",
+      description:
+        "Our school hours are from 8:00 AM to 3:00 PM, Monday to Friday. We also offer extended care options for parents who need early drop-off or late pick-up.",
+      id: 1,
+    },
+    {
+      title: "Is there a uniform policy for students?",
+      description:
+        "Our school hours are from 8:00 AM to 3:00 PM, Monday to Friday. We also offer extended care options for parents who need early drop-off or late pick-up.",
+      id: 2,
+    },
+    {
+      title: "What extracurricular activities are available for students?",
+      description:
+        "Our school hours are from 8:00 AM to 3:00 PM, Monday to Friday. We also offer extended care options for parents who need early drop-off or late pick-up.",
+      id: 3,
+    },
+
+    {
+      title: "What extracurricular activities are available for students?",
+      description:
+        "Our school hours are from 8:00 AM to 3:00 PM, Monday to Friday. We also offer extended care options for parents who need early drop-off or late pick-up.",
+      id: 4,
+    },
+  ];
+
+ export const faqData2 = [
+    {
+      title: "How do you handle food allergies and dietary restrictions?",
+      description:
+        "Our school hours are from 8:00 AM to 3:00 PM, Monday to Friday. We also offer extended care options for parents who need early drop-off or late pick-up.",
+      id: 1,
+    },
+    {
+      title: "What is the teacher-to-student ratio at Little Learners Academy?",
+      description:
+        "Our school hours are from 8:00 AM to 3:00 PM, Monday to Friday. We also offer extended care options for parents who need early drop-off or late pick-up.",
+      id: 2,
+    },
+    {
+      title: "What extracurricular activities are available for students?",
+      description:
+        "Our school hours are from 8:00 AM to 3:00 PM, Monday to Friday. We also offer extended care options for parents who need early drop-off or late pick-up.",
+      id: 3,
+    },
+
+    {
+      title: "How do you handle discipline and behavior management?",
+      description:
+        "Our school hours are from 8:00 AM to 3:00 PM, Monday to Friday. We also offer extended care options for parents who need early drop-off or late pick-up.",
+      id: 4,
+    },
+  ];
+
+ export const detail1 = [
+    {
+      title: "About Us",
+      description:
+        "Discover our Mission, Values, and our unwavering commitment to providing the best learning experience for your child. Learn about our passionate educators and our engaging approach to early education.",
+      id: 1,
+    },
+    {
+      title: "Academics",
+      description:
+        "Delve into our comprehensive academic programs designed to stimulate young minds, foster creativity, and encourage a love for learning. Explore our well-rounded curriculum that nurtures both intellectual and social development.",
+      id: 2,
+    },
+  ];
+
+ export const detail2 = [
+    {
+      title: "Student Life",
+      description:
+        "Peek into the vibrant and enriching world of Student Life at Little Learners Academy. Discover the array of extracurricular activities, arts and crafts, sports, and social events that make our school experience truly memorable.",
+      id: 1,
+    },
+    {
+      title: "Admissions",
+      description:
+        "Learn about our Enrollment Process and how to secure your child's place at Little Learners Academy. Find information about our admission requirements, application deadlines, and available spaces.",
+      id: 2,
+    },
+  ];
+
+
+//   export default {detail1,detail2,cardsData1,cardsData2,avatarData1,faqData1,faqData2 }
