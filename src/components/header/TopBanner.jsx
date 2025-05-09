d7c28792fbbd2477e55ed0e3492b80f1472e2d3c
lizzy-km <oxygenisme.02@gmail.com>
2025-05-09 20:33:48 +0630

    change path

--- src/components/header/TopBanner.jsx
+++ src/components/header/TopBanner.jsx
@@ -3,10 +3,10 @@ import ArrowLeft from "/src/assets/arrowLeft.svg?react";
 const TopBanner = () => {
   return (
     <div  className=" max-w-full overflow-hidden  relative text-grey flex py-[14px] gap-[14px] cursor-pointer w-full rounded-[8px] bg-secondary justify-center items-center self-stretch ">
-         <img className=" absolute left-[-9px] w-auto h-full " src="/src/assets/banner01.svg" alt="top banner image"  />
-         <img className=" absolute right-[-9px] w-auto h-full " src="/src/assets/banner02.svg" alt="top banner image"  />
-         <img className=" absolute left-[75%] w-[39.55px] h-[39.59px] top-[29px] " src="/src/assets/banner03.svg" alt="top banner image"  />
-         <img className=" absolute left-[25%] top-[-13px]  w-[72.3px] h-[72.4px] " src="/src/assets/banner04.svg" alt="top banner image"  />
+         <img className=" absolute left-[-9px] w-auto h-full " src="../../../src/assets/banner01.svg" alt="top banner image"  />
+         <img className=" absolute right-[-9px] w-auto h-full " src="../../../src/assets/banner02.svg" alt="top banner image"  />
+         <img className=" absolute left-[75%] w-[39.55px] h-[39.59px] top-[29px] " src="../../../src/assets/banner03.svg" alt="top banner image"  />
+         <img className=" absolute left-[25%] top-[-13px]  w-[72.3px] h-[72.4px] " src="../../../src/assets/banner04.svg" alt="top banner image"  />
 
 
 
